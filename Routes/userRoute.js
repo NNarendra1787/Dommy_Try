6bde9fc95da48dbd3a828e04ea6fb1bafd61272b
Narendra <narendranarwade2002@gmail.com>
2024-03-05 19:41:25 +0530

    sec

--- Routes/userRoute.js
+++
@@ -1,8 +0,0 @@
-const { RegisterUser, LoginUser } = require('../Controller/userCont');
-
-const router = require('express').Router();
-
-router.post("/register", RegisterUser)
-router.post("/login", LoginUser)
-// router.post("/nok", newData)
-module.exports = router
